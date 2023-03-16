c58483a1eeaa1d8146846c4baa01057423a29111
Nathan Pham <nathan.pham@legato.co>
2023-03-16 09:44:27 +0700

    change the environment file name

--- bricks/ddd/__brick__/{{name}}/infrastructure/repositories/{{name.snakeCase()}}_repository.dart
+++ bricks/ddd/__brick__/{{name}}/infrastructure/repositories/{{name.snakeCase()}}_repository.dart
@@ -3,7 +3,7 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
 import '../../domain/interfaces/{{name.snakeCase()}}_interface.dart';
-import '../../../../common/utils/environment.dart';
+import '../../../../common/utils/app_environment.dart';
 import '../../../../core/infrastructure/datasources/remote/api/base/api_error.dart';
 import '../../../{{name.snakeCase()}}/domain/entities/{{name.snakeCase()}}.dart';
 
