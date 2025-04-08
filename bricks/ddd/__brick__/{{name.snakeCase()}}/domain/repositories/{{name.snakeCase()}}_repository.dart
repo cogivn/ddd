a901268111d70b7817c71d627f393ad664226818
Nathan Pham <nathan.pham@legato.co>
2025-04-08 11:25:35 +0700

    refactor: add interface class for repository

--- bricks/ddd/__brick__/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart
+++ bricks/ddd/__brick__/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart
@@ -4,7 +4,7 @@ import 'package:result_dart/result_dart.dart';
 import '../../../../core/infrastructure/datasources/remote/api/base/api_error.dart';
 import '../../../{{name.snakeCase()}}/domain/entities/{{name.snakeCase()}}.dart';
 
-abstract class {{name.pascalCase()}}Repository {
+abstract interface class {{name.pascalCase()}}Repository {
   Future<ResultDart<{{name.pascalCase()}}, ApiError>> getById(int id, {CancelToken? token});
 }
 
